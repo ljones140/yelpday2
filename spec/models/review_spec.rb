31c001e0b8b1ebdeb425a6db35d074a16ff84113
Christopher Alcock <christopher.alcock@gmail.com>
2015-09-04 12:09:44 +0100

    added user review relations

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe Review, type: :model do
   it { is_expected.to belong_to :restaurant}
+  it { is_expected.to belong_to :user}
+
 end
 
 describe Review, type: :model do
